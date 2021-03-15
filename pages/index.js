7bc1169e320d889df49715bd4e0a898a7025f383
Vitor Margis <vitor.margis@gmail.com>
2021-03-15 16:33:25 +0100

    Quickfix fetch

--- pages/index.js
+++ pages/index.js
@@ -31,7 +31,7 @@ function Home({ options }) {
 }
 
 export async function getServerSideProps() {
-  const res = await fetch(`${process.env.baseUrl || 'http://localhost:3000'}/api/search`)
+  const res = await fetch(`${ process.env.baseUrl || 'http://localhost:3000' }/api/search`)
   const options = await res.json()
   return { props: { options } }
 }
